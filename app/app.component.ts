ea73519989079a914a847158cbee51fb0e0ad2a5
Alex <alexshtanko@gmail.com>
2017-06-01 23:39:08 +0300

    Create module, Component, main

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by alex on 01.06.2017.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
+})
+
+export class AppComponent {
+    title = 'Angular 2do1'
+
+}
---
+++ app/main.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by alex on 01.06.2017.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
